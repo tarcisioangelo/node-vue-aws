8940c5fbb2eefae168a8c70db487b621590b78c6
fimartins <filipe.signal@gmail.com>
2024-10-30 14:45:35 -0300

    validacoes login

--- frontend/src/views/Login.vue
+++ frontend/src/views/Login.vue
@@ -2,29 +2,36 @@
     <div class="w-full my-auto">
         <p class="text-[36px]">Login</p>
 
-        <form autocomplete="off">
+        <form @submit.prevent="handleLogin" autocomplete="off">
             <FormContainer>
                 <div class="col-span-12">
                     <label class="text-white/75" for="email">Email</label>
                     <input
+                        v-model="userAuth.email"
                         class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
                         type="text"
                         id="email"
                         name="email"
                         placeholder="teste@gmail.com"
-                        v-model="email"
                     />
+
+                    <div v-if="v$.email.$error" class="text-red-500">
+                        {{ v$.email.$errors[0].$message }}
+                    </div>
                 </div>
                 <div class="col-span-12">
                     <label class="text-white/75" for="email">Senha</label>
                     <input
+                        v-model="userAuth.password"
                         class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
                         type="password"
                         id="password"
                         name="password"
                         placeholder="digite sua senha"
-                        v-model="password"
                     />
+                    <div v-if="v$.password.$error" class="text-red-500">
+                        {{ v$.password.$errors[0].$message }}
+                    </div>
                 </div>
             </FormContainer>
             <div class="w-64 m-auto text-center pt-4">
@@ -38,8 +45,9 @@
 <script lang="ts">
 import FormContainer from '@/components/Form/FormContainer.vue'
 import DefaultButton from '@/components/Buttons/DefaultButton.vue'
-import { defineComponent, ref } from 'vue'
-// import DefaultCard from '@/components/DefaultCard.vue'
+import { computed, defineComponent, reactive } from 'vue'
+import { helpers, email, required } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core'
 
 export default defineComponent({
     components: {
@@ -47,10 +55,32 @@ export default defineComponent({
         DefaultButton,
     },
     setup() {
-        const email = ref('')
-        const password = ref<string>('')
+        const userAuth = reactive({
+            email: '',
+            password: '',
+        })
+        const rules = computed(() => ({
+            email: {
+                required: helpers.withMessage('Campo obrigatório', required),
+                email: helpers.withMessage('Email inválido', email),
+            },
+            password: {
+                required: helpers.withMessage('Campo obrigatório', required),
+            },
+        }))
 
-        return { email, password }
+        const v$ = useVuelidate(rules, userAuth)
+
+        const handleLogin = async () => {
+            const isValid = await v$.value.$validate()
+            if (isValid) {
+                console.log('Formulário válido:', userAuth)
+            } else {
+                console.log('Formulário inválido')
+            }
+        }
+
+        return { userAuth, v$, handleLogin }
     },
 })
 </script>
