8ea932459beea6a358851a4ea50bfcab7fd4784c
fimartins <filipe.signal@gmail.com>
2024-10-31 18:36:06 -0300

    validacao no register funcionando

--- src/views/Register.vue
+++ src/views/Register.vue
@@ -4,59 +4,60 @@
         <p class="text-center text-md font-light pb-4">Crie sua conta e use todas as funcionalidades sem custo.</p>
         <FormContainer>
             <div class="col-span-12 md:col-span-6">
-                <label class="text-white/75" for="name">Nome</label>
-                <input
-                    class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
-                    type="text"
+                <DefaultInputText
+                    required
+                    v-model="newUser.name"
                     id="name"
                     name="name"
+                    label="Nome"
                     placeholder="Digite seu nome"
-                    autocomplete="off"
-                    v-model="name"
+                    :v$="v$"
                 />
             </div>
             <div class="col-span-12 md:col-span-6">
-                <label class="text-white/75" for="lastName">Sobrenome</label>
-                <input
-                    class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
-                    type="text"
-                    id="last_Name"
-                    name="last_Name"
+                <DefaultInputText
+                    required
+                    v-model="newUser.lastName"
+                    id="lastName"
+                    name="lastName"
+                    label="Sobrenome"
                     placeholder="Digite seu sobrenome"
-                    v-model="last_Name"
+                    :v$="v$"
                 />
             </div>
             <div class="col-span-12">
-                <label class="text-white/75" for="email">Email</label>
-                <input
-                    class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
-                    type="text"
+                <DefaultInputText
+                    required
+                    v-model="newUser.email"
                     id="email"
                     name="email"
+                    label="Email"
                     placeholder="teste@gmail.com"
-                    v-model="email"
+                    :v$="v$"
                 />
             </div>
             <div class="col-span-12 md:col-span-6">
-                <label class="text-white/75" for="password">Senha</label>
-                <input
-                    class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
-                    type="password"
+                <DefaultInputText
+                    required
+                    v-model="newUser.password"
                     id="password"
                     name="password"
-                    placeholder="Digite sua senha"
-                    v-model="password"
+                    type="password"
+                    label="Senha"
+                    placeholder="Digite uma senha"
+                    :v$="v$"
                 />
             </div>
             <div class="col-span-12 md:col-span-6">
-                <label class="text-white/75" for="password_confirm">Confirmação de senha</label>
-                <input
-                    class="w-full my-2 bg-input h-[50px] rounded-[10px] outlined-none px-4 focus:bg-input-line outline-none"
+                <DefaultInputText
+                    required
+                    v-model="newUser.passwordConfirm"
+                    id="passwordConfirm"
+                    name="passwordConfirm"
                     type="password"
-                    id="password_confirm"
-                    name="password_confirm"
-                    placeholder="Confirme sua senha"
-                    v-model="password_confirm"
+                    label="Confirmação de Senha"
+                    placeholder="Confirme a senha"
+                    :v$="v$"
                 />
             </div>
         </FormContainer>
@@ -68,30 +69,65 @@
     </form>
 </template>
 <script lang="ts">
-import { defineComponent, ref } from 'vue'
+import { computed, defineComponent, reactive } from 'vue'
 import { useRouter } from 'vue-router'
 import DefaultButton from '@/components/Buttons/DefaultButton.vue'
 import FormContainer from '@/components/Form/FormContainer.vue'
+import DefaultInputText from '@/components/Form/DefaultInputText.vue'
+import { helpers, required, email, sameAs } from '@vuelidate/validators'
+import useVuelidate from '@vuelidate/core'
 
 export default defineComponent({
     components: {
         DefaultButton,
         FormContainer,
+        DefaultInputText,
     },
     setup() {
         const router = useRouter()
 
-        const name = ref<string>('')
-        const last_Name = ref<string>('')
-        const email = ref('')
-        const password = ref<string>('')
-        const password_confirm = ref<string>('')
+        const newUser = reactive({
+            name: '',
+            lastName: '',
+            email: '',
+            password: '',
+            passwordConfirm: '',
+        })
+        const rules = computed(() => ({
+            name: {
+                required: helpers.withMessage('Campo obrigatório', required),
+            },
+            lastName: {
+                required: helpers.withMessage('Campo obrigatório', required),
+            },
+            email: {
+                required: helpers.withMessage('Campo obrigatório', required),
+                email: helpers.withMessage('Email inválido', email),
+            },
+            password: {
+                required: helpers.withMessage('Campo obrigatório', required),
+            },
+            passwordConfirm: {
+                required: helpers.withMessage('Campo obrigatório', required),
+                sameAsPassword: helpers.withMessage(
+                    'As senhas devem ser iguais',
+                    sameAs(computed(() => newUser.password))
+                ),
+            },
+        }))
 
-        const handleSubmit = () => {
-            console.log('TESTE SUBMISSAO')
+        const v$ = useVuelidate(rules, newUser)
+
+        const handleSubmit = async () => {
+            const isValid = await v$.value.$validate()
+            if (isValid) {
+                console.log('Formulário válido:', newUser)
+            } else {
+                console.log('Formulário inválido')
+            }
         }
 
-        return { name, last_Name, email, password, password_confirm, router, handleSubmit }
+        return { newUser, router, v$, handleSubmit }
     },
 })
 </script>
