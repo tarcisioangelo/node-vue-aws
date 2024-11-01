615803d83cd535b68d0abc10c123c977e5cd99ea
fimartins <filipe.signal@gmail.com>
2024-11-01 15:43:38 -0300

    melhorias logo

--- src/components/Navigation/CustomHeader.vue
+++ src/components/Navigation/CustomHeader.vue
@@ -1,9 +1,17 @@
 <template>
-    <header class="custom-header">Header tasks</header>
+    <header class="custom-header">
+        <div class="logo-box">
+            <img src="/logo.png" width="40px" height="40px" />
+            <label class="font-bold">USERTASKS</label>
+        </div>
+    </header>
 </template>
 
 <style scoped>
 .custom-header {
-    @apply bg-component  h-[68px] shadow-2xl flex justify-between items-center p-3 text-3xl font-semibold text-white;
+    @apply bg-component  h-[68px] shadow-2xl flex justify-between items-center py-3 px-8 text-3xl font-semibold text-white;
+}
+.logo-box {
+    @apply flex items-center gap-3 cursor-pointer;
 }
 </style>
